0031300bc0bfe72d5b9ba401f771fc0373117d65
Andrew Morrison <morro141@gmail.com>
2016-03-01 15:18:51 -0400

    Fully functional to do list

--- toDoList/SecondViewController.swift
+++ toDoList/SecondViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var item: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,26 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func addItem(sender: AnyObject) {
+        
+        toDoList.append(item.text!)
+        item.text = ""
+        NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+        
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        
+        item.resignFirstResponder()
+        return true
+        
+    }
+    
 }
 
